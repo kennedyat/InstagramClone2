c8253b2e356fbe4c40f160ff6523cea9a4fc8f5c
kennedyat <ktpenny8@gmail.com>
2021-10-20 23:00:30 -0400

    Unit 5

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -13,8 +13,9 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("qqdfh5jUjj4pPSRL23JwUOzuCMB89VUhloybPUER") // should correspond to Application Id env variable
-                .clientKey("TdkklrceeBqXg7rO7PsL7v6LuHyNuPnxRizDPOxf")  // should correspond to Client key env variable
+                .applicationId("GufQ9v5JZvX9C8ZEU0BJh49N2SdMNNPDNpWbklC5")
+                .clientKey("BAGFZsSKuXctnlclgGUGdNVzc8KpyME8YSGORngq")
+                 // should correspond to Client key env variable
                 .server("https://parseapi.back4app.com").build());
 
     }
